84ad00694e6a1c9e1745dd674af73f7110a88dba
Balthazar GRONON <bgronon@gmail.com>
2014-02-01 14:37:50 +0100

    name

--- js/content.js
+++ js/content.js
@@ -2,7 +2,7 @@ $(document).on('DOMNodeInserted', function(e) {
 	if (e.target.className == "toolbar")
 	{
 		$("code").addClass("language-clike");
-		$("code").wrap("<pre class='language-clike'></pre>");;
+		$("code").wrap("<pre class='language-clike line-numbers'></pre>");;
 		Prism.highlightAll();
 	}
 });
